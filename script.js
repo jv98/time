f1d07a91130dcb7204daaf0db9fe91ef0e50c65c
jv98 <jacob@vahlen.se>
2020-11-05 15:05:23 +0100

    cuntdown timer

---
+++ script.js
@@ -0,0 +1,30 @@
+const dagarEl = document.getElementById("days");
+const timarEl = document.getElementById("hours");
+const minuterEl = document.getElementById("min");
+const sekunderEl = document.getElementById("seconds");
+
+const datetime = "11 June 2021";
+
+function countdown() {
+    const tiemdate = new Date (datetime);
+    const nudate = new Date();
+
+    const hellaseconds = new Date(tiemdate - nudate) / 1000;
+
+    const dagar = Math.floor(hellaseconds / 3600/ 24);
+    const timar = Math.floor(hellaseconds / 3600) % 24;
+    const minuter= Math.floor(hellaseconds / 60) % 60;
+    const sekunder = Math.floor(hellaseconds) % 60;
+  
+    dagarEl.innerHTML = dagar;
+    timarEl.innerHTML = formatTime(timar);
+    minuterEl.innerHTML = formatTime(minuter);
+    sekunderEl.innerHTML = formatTime(sekunder);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+countdown();
+
+setInterval (countdown, 1000);
